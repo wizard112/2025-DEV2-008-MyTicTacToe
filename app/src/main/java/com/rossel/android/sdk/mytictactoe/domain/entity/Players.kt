2a52ec991d3697d929bba38600580e8f2a01a29c
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-06 21:13:35 +0200

    feat(): implement the class players

---
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/Players.kt
@@ -0,0 +1,5 @@
+package com.rossel.android.sdk.mytictactoe.domain.entity
+
+class Players {
+    val playerX = "X"
+}
--- app/src/test/java/com/rossel/android/sdk/mytictactoe/PlayersTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/PlayersTest.kt
@@ -1,9 +1,12 @@
 package com.rossel.android.sdk.mytictactoe
 
+import com.rossel.android.sdk.mytictactoe.domain.entity.Players
 import org.junit.Assert
 import org.junit.Test
 
 class PlayersTest {
+
+    private val players: Players = Players()
 
     @Test
     fun `should have the player X and player O when start the game`() {
