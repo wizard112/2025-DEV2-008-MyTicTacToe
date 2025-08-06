d6e34fd904d8b0bf8a04c9055eaa2acda28430df
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-06 20:29:06 +0200

    feat(): implement the rule player X always goes first

--- app/src/test/java/com/rossel/android/sdk/mytictactoe/GameRulesTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/GameRulesTest.kt
@@ -1,12 +1,15 @@
 package com.rossel.android.sdk.mytictactoe
 
+import com.rossel.android.sdk.mytictactoe.domain.entity.Game
 import org.junit.Assert
 import org.junit.Test
 
 class GameRulesTest {
 
+    private val game: Game = Game()
+
     @Test
     fun `should player X when the game starts`() {
-        Assert.assertTrue(currentPlayer())
+        Assert.assertEquals("X", game.currentPlayer())
     }
 }
