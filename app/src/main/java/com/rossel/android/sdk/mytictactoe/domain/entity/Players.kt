b8d7b1c1871b6c16f717751442f261aff2765e47
Your Name <gael@MacBook-Pro-M1-de-Gael-Tshilombo.local>
2025-08-06 21:24:10 +0200

    refactor(): use an interface for abstract (SOLID Principles)

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/Players.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/Players.kt
@@ -1,11 +1,13 @@
 package com.rossel.android.sdk.mytictactoe.domain.entity
 
-class Players {
+import com.rossel.android.sdk.mytictactoe.domain.IPlayer
+
+class Players: IPlayer {
     val playerX = "X"
     val playerO = "O"
     var current: String = ""
 
-    fun turnTo() {
+    override fun turnTo() {
         current = when {
             current.isEmpty() -> playerX
             else -> {
---
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/interface/IPlayer.kt
@@ -0,0 +1,5 @@
+package com.rossel.android.sdk.mytictactoe.domain.interface
+
+interface IPlayer {
+    fun turnTo()
+}
