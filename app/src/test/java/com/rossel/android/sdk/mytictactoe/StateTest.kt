e5890707408d78ddfb000152edf8b66507f69765
DEV2-008 <DEV2-008>
2025-08-07 23:22:08 +0200

    feat(): implement class State of board

--- app/src/test/java/com/rossel/android/sdk/mytictactoe/StateTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/StateTest.kt
@@ -1,9 +1,11 @@
 package com.rossel.android.sdk.mytictactoe
 
+import com.rossel.android.sdk.mytictactoe.domain.entity.GameState
 import org.junit.Assert
 import org.junit.Test
 
 class StateTest {
+    private val state = GameState()
 
     @Test
     fun `should finished when player has three in row of first horizontal`() {
