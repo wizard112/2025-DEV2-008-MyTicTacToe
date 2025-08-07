3cf888609552bfd7737ec81570a95edbe378fe2b
DEV2-008 <DEV2-008>
2025-08-07 23:34:45 +0200

    refactor(): move logic state from game to GameState

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/GameState.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/GameState.kt
@@ -1,5 +1,42 @@
 package com.rossel.android.sdk.mytictactoe.domain.entity
 
+import com.rossel.android.sdk.mytictactoe.data.exceptions.GameException
+import com.rossel.android.sdk.mytictactoe.domain.constants.FIELD_ALREADY_TAKEN
+import com.rossel.android.sdk.mytictactoe.domain.enums.Player
+
 class GameState {
-    fun current(): String = "finished"
+    val board: MutableList<Player> = MutableList(9, { Player.EMPTY })
+
+    fun moveTo(position: Int, player: Player) {
+        if (board[position] != Player.EMPTY)
+            throw GameException(msg = FIELD_ALREADY_TAKEN)
+        board[position] = player
+    }
+
+    fun current(player: Player): String = if (horizontalRow(player = player)
+        || verticalRow(player = player)
+        || diagonalRow(player = player)
+        || antidiagonalRow(player = player)) {
+        "finished"
+    } else "match nul"
+
+    private fun horizontalRow(player: Player): Boolean {
+        return (board[0] == player && board[1] == player && board[2] == player)
+                || (board[3] == player && board[4] == player && board[5] == player)
+                || (board[6] == player && board[7] == player && board[8] == player)
+    }
+
+    private fun verticalRow(player: Player): Boolean {
+        return (board[0] == player && board[3] == player && board[6] == player)
+                || (board[1] == player && board[4] == player && board[7] == player)
+                || (board[2] == player && board[5] == player && board[8] == player)
+    }
+
+    private fun diagonalRow(player: Player): Boolean {
+        return (board[0] == player && board[4] == player && board[8] == player)
+    }
+
+    private fun antidiagonalRow(player: Player): Boolean {
+        return (board[2] == player && board[4] == player && board[6] == player)
+    }
 }
--- app/src/test/java/com/rossel/android/sdk/mytictactoe/StateTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/StateTest.kt
@@ -1,6 +1,7 @@
 package com.rossel.android.sdk.mytictactoe
 
 import com.rossel.android.sdk.mytictactoe.domain.entity.GameState
+import com.rossel.android.sdk.mytictactoe.domain.enums.Player
 import org.junit.Assert
 import org.junit.Test
 
@@ -9,6 +10,81 @@ class StateTest {
 
     @Test
     fun `should finished when player has three in row of first horizontal`() {
-        Assert.assertEquals("finished", state.current())
+        state.moveTo(position = 0, player = Player.X)
+        state.moveTo(position = 3, player = Player.O)
+        state.moveTo(position = 1, player = Player.X)
+        state.moveTo(position = 6, player = Player.O)
+        state.moveTo(position = 2, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
+    }
+
+    @Test
+    fun `should finished when player has three in row of second horizontal`() {
+        state.moveTo(position = 3, player = Player.X)
+        state.moveTo(position = 0, player = Player.O)
+        state.moveTo(position = 4, player = Player.X)
+        state.moveTo(position = 1, player = Player.O)
+        state.moveTo(position = 5, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
+    }
+
+    @Test
+    fun `should finished when player has three in row of third horizontal`() {
+        state.moveTo(position = 6, player = Player.X)
+        state.moveTo(position = 0, player = Player.O)
+        state.moveTo(position = 7, player = Player.X)
+        state.moveTo(position = 1, player = Player.O)
+        state.moveTo(position = 8, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
+    }
+
+    @Test
+    fun `should finished when player has three in row of first vertical`() {
+        state.moveTo(position = 0, player = Player.X)
+        state.moveTo(position = 1, player = Player.O)
+        state.moveTo(position = 3, player = Player.X)
+        state.moveTo(position = 2, player = Player.O)
+        state.moveTo(position = 6, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
+    }
+
+    @Test
+    fun `should finished when player has three in row of second vertical`() {
+        state.moveTo(position = 1, player = Player.X)
+        state.moveTo(position = 0, player = Player.O)
+        state.moveTo(position = 4, player = Player.X)
+        state.moveTo(position = 2, player = Player.O)
+        state.moveTo(position = 7, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
+    }
+
+    @Test
+    fun `should finished when player has three in row of third vertical`() {
+        state.moveTo(position = 1, player = Player.X)
+        state.moveTo(position = 0, player = Player.O)
+        state.moveTo(position = 4, player = Player.X)
+        state.moveTo(position = 2, player = Player.O)
+        state.moveTo(position = 7, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
+    }
+
+    @Test
+    fun `should finished when player has three in diagonal`() {
+        state.moveTo(position = 0, player = Player.X)
+        state.moveTo(position = 1, player = Player.O)
+        state.moveTo(position = 4, player = Player.X)
+        state.moveTo(position = 2, player = Player.O)
+        state.moveTo(position = 8, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
+    }
+
+    @Test
+    fun `should finished when player has three in antidiagonal`() {
+        state.moveTo(position = 2, player = Player.X)
+        state.moveTo(position = 1, player = Player.O)
+        state.moveTo(position = 4, player = Player.X)
+        state.moveTo(position = 3, player = Player.O)
+        state.moveTo(position = 6, player = Player.X)
+        Assert.assertEquals("finished", state.current(player = Player.X))
     }
 }
