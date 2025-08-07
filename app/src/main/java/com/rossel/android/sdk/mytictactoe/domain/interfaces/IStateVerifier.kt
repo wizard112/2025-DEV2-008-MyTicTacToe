7f400c2d86694c4a5a74d2282c61d0082bff0380
DEV2-008 <DEV2-008>
2025-08-08 00:20:11 +0200

    refactor(): use the abstraction for Verifier Game

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/interfaces/IStateVerifier.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/interfaces/IStateVerifier.kt
@@ -1,7 +1,6 @@
 package com.rossel.android.sdk.mytictactoe.domain.interfaces
 
-import com.rossel.android.sdk.mytictactoe.domain.entity.VerifierGame
 
 interface IStateVerifier {
-    fun verifier(): VerifierGame
+    fun verifier(): IVerifier
 }
