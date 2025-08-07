81f904843b8af5f3105ad32a9f47696e1900e291
DEV2-008 <DEV2-008>
2025-08-07 23:57:57 +0200

    feat(): implement verify the state of game/board

---
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/VerifierGame.kt
@@ -0,0 +1,6 @@
+package com.rossel.android.sdk.mytictactoe.domain.entity
+
+class VerifierGame {
+
+    fun verify(): String = "finished"
+}
--- app/src/test/java/com/rossel/android/sdk/mytictactoe/VerifierGameTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/VerifierGameTest.kt
@@ -1,9 +1,12 @@
 package com.rossel.android.sdk.mytictactoe
 
+import com.rossel.android.sdk.mytictactoe.domain.entity.VerifierGame
 import org.junit.Assert
 import org.junit.Test
 
 class VerifierGameTest {
+    private val verifier = VerifierGame()
+
     @Test
     fun `should finished when player has three in row of first horizontal`() {
         Assert.assertEquals("finished", verifier.verify())
