1b6b5f97f6489eba94f97f0e4d7170c8a6a2879d
DEV2-008 <DEV2-008>
2025-08-08 00:16:20 +0200

    refactor(): the test for verifier

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/VerifierGame.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/VerifierGame.kt
@@ -1,6 +1,37 @@
 package com.rossel.android.sdk.mytictactoe.domain.entity
 
+import com.rossel.android.sdk.mytictactoe.domain.enums.Player
+
 class VerifierGame {
 
-    fun verify(): String = "finished"
+    fun verify(board: List<Player>): String = if (horizontalRow(board = board, player = Player.X)
+        || horizontalRow(board = board, player = Player.O)
+        || verticalRow(board = board, player = Player.X)
+        || verticalRow(board = board, player = Player.O)
+        || diagonalRow(board = board, player = Player.X)
+        || diagonalRow(board = board, player = Player.O)
+        || antidiagonalRow(board = board, player = Player.X)
+        || antidiagonalRow(board = board, player = Player.O)) {
+        "finished"
+    } else "match nul"
+
+    private fun horizontalRow(board: List<Player>, player: Player): Boolean {
+        return (board[0] == player && board[1] == player && board[2] == player)
+                || (board[3] == player && board[4] == player && board[5] == player)
+                || (board[6] == player && board[7] == player && board[8] == player)
+    }
+
+    private fun verticalRow(board: List<Player>, player: Player): Boolean {
+        return (board[0] == player && board[3] == player && board[6] == player)
+                || (board[1] == player && board[4] == player && board[7] == player)
+                || (board[2] == player && board[5] == player && board[8] == player)
+    }
+
+    private fun diagonalRow(board: List<Player>, player: Player): Boolean {
+        return (board[0] == player && board[4] == player && board[8] == player)
+    }
+
+    private fun antidiagonalRow(board: List<Player>, player: Player): Boolean {
+        return (board[2] == player && board[4] == player && board[6] == player)
+    }
 }
--- app/src/test/java/com/rossel/android/sdk/mytictactoe/VerifierGameTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/VerifierGameTest.kt
@@ -1,14 +1,105 @@
 package com.rossel.android.sdk.mytictactoe
 
 import com.rossel.android.sdk.mytictactoe.domain.entity.VerifierGame
+import com.rossel.android.sdk.mytictactoe.domain.enums.Player
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class VerifierGameTest {
     private val verifier = VerifierGame()
+    private val board = mutableListOf<Player>()
+
+    @Before
+    fun setup() {
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+        board.add(Player.EMPTY)
+    }
 
     @Test
     fun `should finished when player has three in row of first horizontal`() {
-        Assert.assertEquals("finished", verifier.verify())
+        board[0] = Player.X
+        board[3] = Player.O
+        board[1] = Player.X
+        board[6] = Player.O
+        board[2] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
+    }
+
+    @Test
+    fun `should finished when player has three in row of second horizontal`() {
+        board[3] = Player.X
+        board[0] = Player.O
+        board[4] = Player.X
+        board[1] = Player.O
+        board[5] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
+    }
+
+    @Test
+    fun `should finished when player has three in row of third horizontal`() {
+        board[6] = Player.X
+        board[0] = Player.O
+        board[7] = Player.X
+        board[1] = Player.O
+        board[8] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
+    }
+
+    @Test
+    fun `should finished when player has three in row of first vertical`() {
+        board[0] = Player.X
+        board[1] = Player.O
+        board[3] = Player.X
+        board[2] = Player.O
+        board[6] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
+    }
+
+    @Test
+    fun `should finished when player has three in row of second vertical`() {
+        board[1] = Player.X
+        board[0] = Player.O
+        board[4] = Player.X
+        board[2] = Player.O
+        board[7] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
+    }
+
+    @Test
+    fun `should finished when player has three in row of third vertical`() {
+        board[1] = Player.X
+        board[0] = Player.O
+        board[4] = Player.X
+        board[2] = Player.O
+        board[7] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
+    }
+
+    @Test
+    fun `should finished when player has three in diagonal`() {
+        board[0] = Player.X
+        board[1] = Player.O
+        board[4] = Player.X
+        board[2] = Player.O
+        board[8] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
+    }
+
+    @Test
+    fun `should finished when player has three in antidiagonal`() {
+        board[2] = Player.X
+        board[1] = Player.O
+        board[4] = Player.X
+        board[3] = Player.O
+        board[6] = Player.X
+        Assert.assertEquals("finished", verifier.verify(board = board))
     }
 }
