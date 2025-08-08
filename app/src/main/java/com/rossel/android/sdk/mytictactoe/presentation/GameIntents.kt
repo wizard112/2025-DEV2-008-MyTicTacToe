b5a9c0f15d6211f218ed7490a87a4251b9be130b
DEV2-008 <DEV2-008>
2025-08-08 16:52:22 +0200

    refactor(): use a data class instead class

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameIntents.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameIntents.kt
@@ -2,5 +2,5 @@ package com.rossel.android.sdk.mytictactoe.presentation
 
 sealed class GameIntents {
     object Starting: GameIntents()
-    class Moving(val position: Int): GameIntents()
+    data class Moving(val position: Int): GameIntents()
 }
