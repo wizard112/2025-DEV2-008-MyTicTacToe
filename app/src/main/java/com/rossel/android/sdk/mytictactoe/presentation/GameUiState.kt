92751de6a3dd77517dfbdc045a6d8f2dd098693b
DEV2-008 <DEV2-008>
2025-08-08 15:47:19 +0200

    refactor(): add board as property

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameUiState.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameUiState.kt
@@ -1,5 +1,7 @@
 package com.rossel.android.sdk.mytictactoe.presentation
 
+import com.rossel.android.sdk.mytictactoe.domain.enums.Player
+
 sealed class GameUiState {
-    object Playing: GameUiState()
+    class Playing(val board: List<Player>): GameUiState()
 }
