9c8557719286dec8318da947b59bc1375f87bd60
DEV2-008 <DEV2-008>
2025-08-08 15:04:54 +0200

    refactor(): add use cases : game and verifier

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameViewModel.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameViewModel.kt
@@ -1,7 +1,19 @@
 package com.rossel.android.sdk.mytictactoe.presentation
 
 import androidx.lifecycle.ViewModel
+import com.rossel.android.sdk.mytictactoe.domain.interfaces.IGameUseCase
+import com.rossel.android.sdk.mytictactoe.domain.interfaces.IVerifierUseCase
+import com.rossel.android.sdk.mytictactoe.domain.usecase.GameUseCase
+import com.rossel.android.sdk.mytictactoe.domain.usecase.VerifierGameUseCase
 
 class GameViewModel: ViewModel() {
-    val state = "Started"
+    private val gameUseCase: IGameUseCase = GameUseCase()
+    private val verifierGameUseCase: IVerifierUseCase = VerifierGameUseCase()
+
+    var state = "Started"
+
+    fun manageGame(position: Int) {
+        gameUseCase.play(position = position)
+        state = verifierGameUseCase.verify(board = gameUseCase.state().board()).name
+    }
 }
