2448147bd5abf6ce7067923b8e83ee2c5fb773ec
DEV2-008 <DEV2-008>
2025-08-09 01:12:57 +0200

    refactor(): use constant for references and add a function to get constraint set

--- app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameScreen.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameScreen.kt
@@ -11,39 +11,18 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.bnp.android.kata.mytictactoe.domain.constants.REF_BOARD
+import com.bnp.android.kata.mytictactoe.domain.constants.REF_BUTTON
+import com.bnp.android.kata.mytictactoe.domain.constants.REF_LOADING
+import com.bnp.android.kata.mytictactoe.domain.constants.REF_PLAYERS
+import com.bnp.android.kata.mytictactoe.domain.constants.REF_STATE
 
 @Composable
 fun GameScreen(viewModel: GameViewModel) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     ConstraintLayout(
-        constraintSet = ConstraintSet {
-            val playersRef = createRefFor(id = "players")
-            val boardRef = createRefFor(id = "board")
-            val stateRef = createRefFor(id = "state")
-            val loadingRef = createRefFor(id = "loading")
-            val restartRef = createRefFor(id = "button")
-
-            constrain(ref = boardRef){
-                centerTo(other = parent)
-            }
-            constrain(ref = playersRef){
-                bottom.linkTo(anchor = boardRef.top, margin = 10.dp)
-                centerHorizontallyTo(other = parent)
-            }
-            constrain(ref = stateRef){
-                bottom.linkTo(anchor = boardRef.top, margin = 10.dp)
-                centerHorizontallyTo(other = parent)
-            }
-            constrain(ref = loadingRef){
-                bottom.linkTo(anchor = boardRef.top, margin = 10.dp)
-                centerHorizontallyTo(other = parent)
-            }
-            constrain(restartRef){
-                top.linkTo(anchor = boardRef.bottom, margin = 10.dp)
-                centerHorizontallyTo(other = parent)
-            }
-        },
+        constraintSet = gameScreenConstraintSet(),
         modifier = Modifier.fillMaxSize()
     ) {
         when(uiState) {
@@ -54,5 +33,33 @@ fun GameScreen(viewModel: GameViewModel) {
             is GameUiState.Winner -> {}
             is GameUiState.Playing -> {}
         }
+    }
+}
+
+private fun gameScreenConstraintSet(): ConstraintSet = ConstraintSet {
+    val playersRef = createRefFor(id = REF_PLAYERS)
+    val boardRef = createRefFor(id = REF_BOARD)
+    val stateRef = createRefFor(id = REF_STATE)
+    val loadingRef = createRefFor(id = REF_LOADING)
+    val restartRef = createRefFor(id = REF_BUTTON)
+
+    constrain(ref = boardRef){
+        centerTo(other = parent)
+    }
+    constrain(ref = playersRef){
+        bottom.linkTo(anchor = boardRef.top, margin = 10.dp)
+        centerHorizontallyTo(other = parent)
+    }
+    constrain(ref = stateRef){
+        bottom.linkTo(anchor = boardRef.top, margin = 10.dp)
+        centerHorizontallyTo(other = parent)
+    }
+    constrain(ref = loadingRef){
+        bottom.linkTo(anchor = boardRef.top, margin = 10.dp)
+        centerHorizontallyTo(other = parent)
+    }
+    constrain(restartRef){
+        top.linkTo(anchor = boardRef.bottom, margin = 10.dp)
+        centerHorizontallyTo(other = parent)
     }
 }
