e8e081d532fcb93db968045bb5b26e6516b4afc8
DEV2-008 <DEV2-008>
2025-08-08 15:05:49 +0200

    refactor(): test the state not finished after the first move

--- app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
@@ -9,7 +9,13 @@ class GameViewModelTest {
     private val gameViewModel = GameViewModel()
 
     @Test
-    fun `should state started when the view model is instantiated`() {
+    fun `should state is started when the view model is instantiated`() {
         Assert.assertEquals("Started", gameViewModel.state)
+    }
+
+    @Test
+    fun `should state is not finished when move in a position`() {
+        gameViewModel.manageGame(position = 1)
+        Assert.assertEquals("not finished", gameViewModel.state)
     }
 }
