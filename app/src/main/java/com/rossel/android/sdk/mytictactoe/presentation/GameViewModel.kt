051e0fff0af84062c0201e28c7b35075e7741d58
DEV2-008 <DEV2-008>
2025-08-08 15:42:04 +0200

    refactor(): use the mutbale state flow to manage the state of ui from view model

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameViewModel.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameViewModel.kt
@@ -5,12 +5,17 @@ import com.rossel.android.sdk.mytictactoe.domain.interfaces.IGameUseCase
 import com.rossel.android.sdk.mytictactoe.domain.interfaces.IVerifierUseCase
 import com.rossel.android.sdk.mytictactoe.domain.usecase.GameUseCase
 import com.rossel.android.sdk.mytictactoe.domain.usecase.VerifierGameUseCase
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class GameViewModel: ViewModel() {
     private val gameUseCase: IGameUseCase = GameUseCase()
     private val verifierGameUseCase: IVerifierUseCase = VerifierGameUseCase()
 
-    var state: GameUiState = GameUiState.Started
+    private val _uiState: MutableStateFlow<GameUiState> = MutableStateFlow(GameUiState.Playing)
+    val uiState: StateFlow<GameUiState> = _uiState
+
 
     fun manageGame(position: Int) {
         gameUseCase.play(position = position)
