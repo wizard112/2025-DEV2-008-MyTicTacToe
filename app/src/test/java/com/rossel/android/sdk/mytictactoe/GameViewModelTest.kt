cd5452977bad6bc924614aed21d1b18a15d22e8b
DEV2-008 <DEV2-008>
2025-08-08 16:16:11 +0200

    test(): add failing test when get state after players take a position

--- app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
@@ -1,6 +1,7 @@
 package com.rossel.android.sdk.mytictactoe
 
 import com.rossel.android.sdk.mytictactoe.domain.enums.Player
+import com.rossel.android.sdk.mytictactoe.domain.enums.StateEnum
 import com.rossel.android.sdk.mytictactoe.presentation.GameIntents
 import com.rossel.android.sdk.mytictactoe.presentation.GameUiState
 import com.rossel.android.sdk.mytictactoe.presentation.GameViewModel
@@ -23,5 +24,18 @@ class GameViewModelTest {
         val uiState = gameViewModel.uiState.value
         Assert.assertTrue(uiState is GameUiState.Playing)
         Assert.assertEquals(Player.X.name, (uiState as GameUiState.Playing).playerName)
+    }
+
+    @Test
+    fun `should positions are filled when the players play`() {
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 0))
+        val uiStateOne = gameViewModel.uiState.value
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 2))
+        val uiStateTwo = gameViewModel.uiState.value
+        Assert.assertTrue(uiStateOne is GameUiState.Playing)
+        Assert.assertTrue(uiStateTwo is GameUiState.Playing)
+        Assert.assertEquals(Player.X.name, (uiStateOne as GameUiState.Playing).playerName)
+        Assert.assertEquals(Player.O.name, (uiStateTwo as GameUiState.Playing).playerName)
+        Assert.assertEquals(StateEnum.NOT_FINISHED, uiStateOne.state)
     }
 }
