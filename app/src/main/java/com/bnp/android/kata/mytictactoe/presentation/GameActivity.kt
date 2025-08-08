52b682b32fb3cde404a5b05d23c725a4b7034584
DEV2-008 <DEV2-008>
2025-08-09 00:53:18 +0200

    feat(): add activity to show UI

---
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameActivity.kt
@@ -0,0 +1,13 @@
+package com.bnp.android.kata.mytictactoe.presentation
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {  }
+    }
+}
