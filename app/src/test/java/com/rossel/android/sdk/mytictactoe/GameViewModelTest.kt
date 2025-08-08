ffc3b3708647950d6833b5b352d472f2ff16d68e
DEV2-008 <DEV2-008>
2025-08-08 16:36:54 +0200

    feat(): add test when the game is draw

--- app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
@@ -72,4 +72,21 @@ class GameViewModelTest {
         Assert.assertEquals(StateEnum.FINISHED, (uiState as GameUiState.Playing).stateEnum)
         Assert.assertEquals(Player.O.name, uiState.playerName)
     }
+
+    @Test
+    fun `should the game is draw when all nine squares are filled but without three x or O consecutive in horizontal - vertical - diagonal or antidiagonal`() {
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 0))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 6))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 3))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 8))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 4))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 5))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 7))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 1))
+        val uiState = gameViewModel.uiState.value
+        Assert.assertTrue(uiState is GameUiState.Playing)
+        Assert.assertEquals(StateEnum.MATCH_NUL, (uiState as GameUiState.Playing).stateEnum)
+        Assert.assertEquals("", uiState.playerName)
+    }
 }
