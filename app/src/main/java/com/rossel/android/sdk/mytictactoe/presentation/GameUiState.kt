b66e83f52bbce4da63bdde811fcaed02ad5af904
DEV2-008 <DEV2-008>
2025-08-08 15:59:57 +0200

    refactor(): add the state Loading

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameUiState.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameUiState.kt
@@ -3,5 +3,6 @@ package com.rossel.android.sdk.mytictactoe.presentation
 import com.rossel.android.sdk.mytictactoe.domain.enums.Player
 
 sealed class GameUiState {
+    object Loading: GameUiState()
     class Playing(val board: List<Player>): GameUiState()
 }
