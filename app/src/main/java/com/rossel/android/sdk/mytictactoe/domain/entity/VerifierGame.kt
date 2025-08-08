495178a0060031aad7aa58f0f1e9eac98171f036
DEV2-008 <DEV2-008>
2025-08-08 14:16:36 +0200

    refactor(): use a class for each type of verifier : clean code

--- app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/VerifierGame.kt
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/domain/entity/VerifierGame.kt
@@ -4,36 +4,49 @@ import com.rossel.android.sdk.mytictactoe.domain.enums.Player
 import com.rossel.android.sdk.mytictactoe.domain.enums.StateEnum
 import com.rossel.android.sdk.mytictactoe.domain.interfaces.IVerifier
 
-class VerifierGame: IVerifier {
 
-    override fun verify(board: List<Player>): StateEnum = if (horizontalRow(board = board, player = Player.X)
-        || horizontalRow(board = board, player = Player.O)
-        || verticalRow(board = board, player = Player.X)
-        || verticalRow(board = board, player = Player.O)
-        || diagonalRow(board = board, player = Player.X)
-        || diagonalRow(board = board, player = Player.O)
-        || antidiagonalRow(board = board, player = Player.X)
-        || antidiagonalRow(board = board, player = Player.O)) {
-        StateEnum.FINISHED
-    } else StateEnum.MATCH_NUL
-
-    private fun horizontalRow(board: List<Player>, player: Player): Boolean {
+class VerifierHorizontalRow {
+    fun horizontalRow(board: List<Player>, player: Player): Boolean {
         return (board[0] == player && board[1] == player && board[2] == player)
                 || (board[3] == player && board[4] == player && board[5] == player)
                 || (board[6] == player && board[7] == player && board[8] == player)
     }
+}
 
-    private fun verticalRow(board: List<Player>, player: Player): Boolean {
+class VerifierVerticalRow {
+    fun verticalRow(board: List<Player>, player: Player): Boolean {
         return (board[0] == player && board[3] == player && board[6] == player)
                 || (board[1] == player && board[4] == player && board[7] == player)
                 || (board[2] == player && board[5] == player && board[8] == player)
     }
+}
 
-    private fun diagonalRow(board: List<Player>, player: Player): Boolean {
+class VerifierAntidiagonalRow {
+    fun diagonalRow(board: List<Player>, player: Player): Boolean {
         return (board[0] == player && board[4] == player && board[8] == player)
     }
+}
 
-    private fun antidiagonalRow(board: List<Player>, player: Player): Boolean {
+class VerifierDiagonalRow {
+    fun antidiagonalRow(board: List<Player>, player: Player): Boolean {
         return (board[2] == player && board[4] == player && board[6] == player)
     }
+}
+
+class VerifierGame: IVerifier {
+    private val verifierHorizontalRow = VerifierHorizontalRow()
+    private val verifierVerticalRow = VerifierVerticalRow()
+    private val verifierAntidiagonalRow = VerifierAntidiagonalRow()
+    private val verifierDiagonalRow = VerifierDiagonalRow()
+
+    override fun verify(board: List<Player>): StateEnum = if (verifierHorizontalRow.horizontalRow(board = board, player = Player.X)
+        || verifierHorizontalRow.horizontalRow(board = board, player = Player.O)
+        || verifierVerticalRow.verticalRow(board = board, player = Player.X)
+        || verifierVerticalRow.verticalRow(board = board, player = Player.O)
+        || verifierAntidiagonalRow.diagonalRow(board = board, player = Player.X)
+        || verifierAntidiagonalRow.diagonalRow(board = board, player = Player.O)
+        || verifierDiagonalRow.antidiagonalRow(board = board, player = Player.X)
+        || verifierDiagonalRow.antidiagonalRow(board = board, player = Player.O)) {
+        StateEnum.FINISHED
+    } else StateEnum.MATCH_NUL
 }
