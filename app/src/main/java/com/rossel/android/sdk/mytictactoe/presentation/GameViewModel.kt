a508f60c63aa817797228f916669c415c9642df2
DEV2-008 <DEV2-008>
2025-08-08 14:57:45 +0200

    feat(): implement the Game View Model and minimum code for the test

---
+++ app/src/main/java/com/rossel/android/sdk/mytictactoe/presentation/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.rossel.android.sdk.mytictactoe.presentation
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+    val state = "Started"
+}
--- app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/rossel/android/sdk/mytictactoe/GameViewModelTest.kt
@@ -1,12 +1,15 @@
 package com.rossel.android.sdk.mytictactoe
 
+import com.rossel.android.sdk.mytictactoe.presentation.GameViewModel
 import org.junit.Assert
 import org.junit.Test
 
 class GameViewModelTest {
 
+    private val gameViewModel = GameViewModel()
+
     @Test
     fun `should state started when the view model is instantiated`() {
-        Assert.assertEquals("Started", viewModel.state)
+        Assert.assertEquals("Started", gameViewModel.state)
     }
 }
