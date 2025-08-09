dd62f72e312b11dbbb7d4391e40da7a03fea866b
DEV2-008 <DEV2-008>
2025-08-09 23:51:32 +0200

    refactor(): adapt ui state from data class to sealed class

--- app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameUiState.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameUiState.kt
@@ -1,13 +1,5 @@
 package com.bnp.android.kata.mytictactoe.presentation
 
-import androidx.compose.runtime.Immutable
-import com.bnp.android.kata.mytictactoe.domain.enums.Player
-
-@Immutable
-data class GameUiState(
-    val loading: Boolean = false,
-    val playerName: String = "",
-    val matchNul: Boolean = false,
-    val winner: Boolean = false,
-    val board: List<Player> = emptyList()
-)
+sealed class GameUiState {
+    object Loading: GameUiState()
+}
