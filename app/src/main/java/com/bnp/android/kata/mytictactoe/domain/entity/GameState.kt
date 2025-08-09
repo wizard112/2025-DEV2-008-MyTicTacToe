37d624ddd11f34063165f7f0f528a9cbb52562c1
DEV2-008 <DEV2-008>
2025-08-09 04:45:54 +0200

    feat(): add an interface to restart the board to empty

--- app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/GameState.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/GameState.kt
@@ -9,6 +9,17 @@ class GameState: IState {
     private val mBoard: MutableList<Player> = MutableList(9, { Player.EMPTY })
 
     override fun board(): List<Player> = this.mBoard
+    override fun restart() {
+        mBoard[0] = Player.EMPTY
+        mBoard[1] = Player.EMPTY
+        mBoard[2] = Player.EMPTY
+        mBoard[3] = Player.EMPTY
+        mBoard[4] = Player.EMPTY
+        mBoard[5] = Player.EMPTY
+        mBoard[6] = Player.EMPTY
+        mBoard[7] = Player.EMPTY
+        mBoard[8] = Player.EMPTY
+    }
 
     override fun moveTo(position: Int, player: Player) {
         if (mBoard[position] != Player.EMPTY)
