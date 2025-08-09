e738e1d6e9cfad44e1f1042f55fc5c7a88672e19
DEV2-008 <DEV2-008>
2025-08-10 01:49:59 +0200

    refactor(): new reference of the list to allow compose to recompose the view

--- app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameViewModel.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameViewModel.kt
@@ -10,7 +10,6 @@ import com.bnp.android.kata.mytictactoe.domain.usecase.VerifierGameUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
 class GameViewModel: ViewModel() {
     private val gameUseCase: IGameUseCase = GameUseCase()
@@ -22,20 +21,19 @@ class GameViewModel: ViewModel() {
     fun handleIntents(intent: GameIntents) {
         when(intent) {
             GameIntents.Starting -> manageStarting()
-            GameIntents.Restarting -> {
-                gameUseCase.state().reset()
-                gameUseCase.players().reset()
-                manageStarting()
-            }
+            GameIntents.Restarting -> manageRestarting()
             is GameIntents.Moving -> manageMoving(position = intent.position)
         }
     }
 
     private fun manageStarting() {
-        /*_uiState.update {
-            it.copy(board = gameUseCase.state().board(), playerName = gameUseCase.players().playerX().name, loading = false, matchNul = false, winner = false)
-        }*/
-        _uiState.value = GameUiState.Playing(board = gameUseCase.state().board(), playerName = gameUseCase.players().playerX().name)
+        _uiState.value = GameUiState.Playing(board = gameUseCase.state().board().toList(), playerName = gameUseCase.players().playerX().name)
+    }
+
+    private fun manageRestarting() {
+        gameUseCase.state().reset()
+        gameUseCase.players().reset()
+        manageStarting()
     }
 
     private fun manageMoving(position: Int) {
@@ -44,12 +42,10 @@ class GameViewModel: ViewModel() {
             val state = verifierGameUseCase.verify(board = gameUseCase.state().board())
             when(state) {
                 StateEnum.FINISHED -> {
-                    //_uiState.update { it.copy(playerName = gameUseCase.players().currentPlayer().name, winner = true) }
                     _uiState.value = GameUiState.Winner(winnerName = gameUseCase.players().currentPlayer().name)
                 }
                 StateEnum.NOT_FINISHED -> {
-                   // _uiState.update { it.copy(board = gameUseCase.state().board(), playerName = playerName(stateEnum = state)) }
-                    _uiState.value = GameUiState.Playing(board = gameUseCase.state().board(), playerName = playerName(stateEnum = state))
+                    _uiState.value = GameUiState.Playing(board = gameUseCase.state().board().toList(), playerName = playerName(stateEnum = state))
                 }
                 StateEnum.MATCH_NUL -> {
                     _uiState.value = GameUiState.MatchNull
