f80092108592385a0b52b6e754c7777b71621582
DEV2-008 <DEV2-008>
2025-08-09 23:55:43 +0200

    refactor(): add data class Playing in ui state

--- app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameUiState.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameUiState.kt
@@ -1,5 +1,8 @@
 package com.bnp.android.kata.mytictactoe.presentation
 
+import com.bnp.android.kata.mytictactoe.domain.enums.Player
+
 sealed class GameUiState {
     object Loading: GameUiState()
+    data class Playing(val board: List<Player>, val playerName: String): GameUiState()
 }
