5f13eccc3ed1777c357ced14380966bc402aeceb
DEV2-008 <DEV2-008>
2025-08-10 00:08:32 +0200

    refactor(): add a new ui state for match nul

--- app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameUiState.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameUiState.kt
@@ -6,4 +6,5 @@ sealed class GameUiState {
     object Loading: GameUiState()
     data class Playing(val board: List<Player>, val playerName: String): GameUiState()
     data class Winner(val winnerName: String): GameUiState()
+    object MatchNull: GameUiState()
 }
