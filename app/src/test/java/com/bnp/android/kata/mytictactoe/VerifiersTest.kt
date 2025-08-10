5b5b845607c18f6f6d6aa37980d32cac3097da5b
DEV2-008 <DEV2-008>
2025-08-10 18:02:00 +0200

    feat(): implement verifier horizontal row - change parameter of function

--- app/src/test/java/com/bnp/android/kata/mytictactoe/VerifiersTest.kt
+++ app/src/test/java/com/bnp/android/kata/mytictactoe/VerifiersTest.kt
@@ -12,9 +12,16 @@ class VerifiersTest {
     private val boardOnlyRow: HashMap<Int, MutableList<Player>> = hashMapOf()
     private val verifierHorizontalRow: IVerifierRow = VerifierHorizontalRow()
 
+    @Before
+    fun setup() {
+        boardOnlyRow.put(0, mutableListOf(Player.X, Player.X, Player.X))
+        boardOnlyRow.put(1, mutableListOf(Player.X, Player.O, Player.EMPTY))
+        boardOnlyRow.put(2, mutableListOf(Player.X, Player.X, Player.X))
+    }
+
     @Test
     fun `should has 1 2 3 when try to get first row of the map`() {
-        val state = verifierHorizontalRow.verifierRow(board = boardOnlyRow.get(0)?.toList()!!, player = Player.X)
-        Assert.assertEquals(StateEnum.FINISHED, state)
+        val state = verifierHorizontalRow.verifierRow(board = boardOnlyRow, player = Player.X)
+        Assert.assertEquals(true, state)
     }
 }
