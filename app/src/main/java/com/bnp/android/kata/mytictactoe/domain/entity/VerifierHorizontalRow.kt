cee1587a5a634ff1618331675f17aaa44d9718c5
DEV2-008 <DEV2-008>
2025-08-10 18:10:28 +0200

    refactor(): use the correct index for th elast row

--- app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierHorizontalRow.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierHorizontalRow.kt
@@ -7,7 +7,7 @@ class VerifierHorizontalRow: IVerifierRow {
     override fun verifierRow(board: Map<Int, MutableList<Player>>, player: Player): Boolean {
         val rowOne = board[0]
         val rowTwo = board[1]
-        val rowThree = board[0]
+        val rowThree = board[2]
         return rowOne?.filter { it == player }?.size == 3
                 || rowTwo?.filter { it == player }?.size == 3
                 || rowThree?.filter { it == player }?.size == 3
