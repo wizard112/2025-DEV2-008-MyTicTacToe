f75aed225c23095706bb1a2ccea28c3f61fc9066
DEV2-008 <DEV2-008>
2025-08-10 18:02:37 +0200

    refactor(): change paramter of the function - use a map

--- app/src/main/java/com/bnp/android/kata/mytictactoe/domain/interfaces/IVerifierRow.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/domain/interfaces/IVerifierRow.kt
@@ -3,5 +3,5 @@ package com.bnp.android.kata.mytictactoe.domain.interfaces
 import com.bnp.android.kata.mytictactoe.domain.enums.Player
 
 interface IVerifierRow {
-    fun verifierRow(board: List<Player>, player: Player): Boolean
+    fun verifierRow(board: Map<Int, MutableList<Player>>, player: Player): Boolean
 }
