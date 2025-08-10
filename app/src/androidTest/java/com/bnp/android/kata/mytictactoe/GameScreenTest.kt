10dbbe856b21a27df51fb97bba4b1994fefae48d
DEV2-008 <DEV2-008>
2025-08-10 12:58:37 +0200

    test(): create file to test game screen composable

---
+++ app/src/androidTest/java/com/bnp/android/kata/mytictactoe/GameScreenTest.kt
@@ -0,0 +1,15 @@
+package com.bnp.android.kata.mytictactoe
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class GameScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun testGameScreenComposable() {
+
+    }
+}
