c69d90853b28ffe4a051cd1659a1ab3ffd022206
DEV2-008 <DEV2-008>
2025-08-10 23:45:52 +0200

    refactor(): adapt logic to use new logic: use case game - use case verifier - use case players

--- app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameViewModel.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/presentation/GameViewModel.kt
@@ -2,10 +2,14 @@ package com.bnp.android.kata.mytictactoe.presentation
 
 import androidx.lifecycle.ViewModel
 import com.bnp.android.kata.mytictactoe.data.exceptions.GameException
+import com.bnp.android.kata.mytictactoe.domain.constants.NB_COLUMN
+import com.bnp.android.kata.mytictactoe.domain.constants.NB_ROW
 import com.bnp.android.kata.mytictactoe.domain.enums.StateEnum
 import com.bnp.android.kata.mytictactoe.domain.interfaces.IGameUseCase
+import com.bnp.android.kata.mytictactoe.domain.interfaces.IPlayer
 import com.bnp.android.kata.mytictactoe.domain.interfaces.IVerifierUseCase
 import com.bnp.android.kata.mytictactoe.domain.usecase.GameUseCase
+import com.bnp.android.kata.mytictactoe.domain.usecase.PlayersUseCase
 import com.bnp.android.kata.mytictactoe.domain.usecase.VerifierGameUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -13,8 +17,9 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class GameViewModel: ViewModel() {
-    private val gameUseCase: IGameUseCase = GameUseCase(columns = 3, rows = 3)
+    private val gameUseCase: IGameUseCase = GameUseCase(columns = NB_COLUMN, rows = NB_ROW)
     private val verifierGameUseCase: IVerifierUseCase = VerifierGameUseCase()
+    private val playersUseCase: IPlayer = PlayersUseCase()
 
     private val _uiState: MutableStateFlow<GameUiState> = MutableStateFlow(GameUiState(loading = true))
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
@@ -23,44 +28,39 @@ class GameViewModel: ViewModel() {
         when(intent) {
             GameIntents.Starting -> manageStarting()
             GameIntents.Restarting -> {
-                //gameUseCase.state().reset()
-                //gameUseCase.players().reset()
+                gameUseCase.reset()
+                gameUseCase.reset()
                 manageStarting()
             }
-            is GameIntents.Moving -> manageMoving(position = intent.position)
+            is GameIntents.Moving -> manageMoving(column = intent.column, row = intent.row)
         }
     }
 
     private fun manageStarting() {
+        playersUseCase.turnTo()
         _uiState.update {
-            it.copy(board = gameUseCase.board(), playerName = "X", loading = false, matchNul = false, winner = false)
+            it.copy(board = gameUseCase.board().toMap(), playerName = playersUseCase.currentPlayer().name, loading = false, matchNul = false, winner = false)
         }
     }
 
-    private fun manageMoving(position: Int) {
+    private fun manageMoving(column: Int, row: Int) {
         try {
-            //gameUseCase.play(position = position)
-            //val state = verifierGameUseCase.verify(board = gameUseCase.state().board())
-            /*when(state) {
+            gameUseCase.play(column = column, row = row, player = playersUseCase.currentPlayer())
+            val state = verifierGameUseCase.verify(board = gameUseCase.board())
+            when(state) {
                 StateEnum.FINISHED -> {
-                    _uiState.update { it.copy(playerName = gameUseCase.players().currentPlayer().name, winner = true) }
+                    _uiState.update { it.copy(playerName = playersUseCase.currentPlayer().name, winner = true) }
                 }
                 StateEnum.NOT_FINISHED -> {
-                    _uiState.update { it.copy(board = gameUseCase.state().board(), playerName = playerName(stateEnum = state)) }
+                    playersUseCase.turnTo()
+                    _uiState.update { it.copy(board = gameUseCase.board().toMap(), playerName = playersUseCase.currentPlayer().name) }
                 }
                 StateEnum.MATCH_NUL -> {
                     _uiState.update { it.copy(matchNul = true) }
                 }
-            }*/
+            }
         } catch (ex: GameException) {
             ex.printStackTrace()
         }
     }
-
-    private fun playerName(stateEnum: StateEnum): String = ""/*if (stateEnum == StateEnum.MATCH_NUL) ""
-    else
-        if (gameUseCase.players().currentPlayer() == gameUseCase.players().playerX())
-            gameUseCase.players().playerO().name
-        else
-            gameUseCase.players().playerX().name*/
 }
