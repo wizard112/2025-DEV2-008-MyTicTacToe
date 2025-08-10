d82de28bac0c6f7aba0097ac11f92da65952c280
DEV2-008 <DEV2-008>
2025-08-10 23:26:40 +0200

    refactor(): keep only test for use case play - remove other test / move in verifier game test

--- app/src/test/java/com/bnp/android/kata/mytictactoe/GameRulesTest.kt
+++ app/src/test/java/com/bnp/android/kata/mytictactoe/GameRulesTest.kt
@@ -1,18 +1,14 @@
 package com.bnp.android.kata.mytictactoe
 
 import com.bnp.android.kata.mytictactoe.data.exceptions.GameException
-import com.bnp.android.kata.mytictactoe.domain.usecase.VerifierGameUseCase
 import com.bnp.android.kata.mytictactoe.domain.usecase.GameUseCase
 import com.bnp.android.kata.mytictactoe.domain.enums.Player
 import com.bnp.android.kata.mytictactoe.domain.interfaces.IGameUseCase
-import com.bnp.android.kata.mytictactoe.domain.interfaces.IVerifierUseCase
 import org.junit.Assert
 import org.junit.Test
 
 class GameRulesTest {
-
     private val game: IGameUseCase = GameUseCase(columns = 3, rows = 3)
-    //private val verifier: IVerifierUseCase = VerifierGameUseCase()
 
     @Test
     fun `should player X when the game starts`() {
@@ -57,100 +53,4 @@ class GameRulesTest {
         val positionPlayerO = game.board()[2]?.get(2)
         Assert.assertEquals(Player.O, positionPlayerO)
     }
-
 }
-    /*
-    @Test
-    fun `should match nul when nine squares are filled`() {
-        game.play(position = 0)
-        game.play(position = 2)
-        game.play(position = 6)
-        game.play(position = 3)
-        game.play(position = 8)
-        game.play(position = 4)
-        game.play(position = 5)
-        game.play(position = 7)
-        game.play(position = 1)
-        Assert.assertEquals(StateEnum.MATCH_NUL, verifier.verify(board = game.state().board()))
-    }
-
-    /*@Test
-    fun `should finished when player has three in row of first horizontal`() {
-        game.play(position = 0)
-        game.play(position = 3)
-        game.play(position = 1)
-        game.play(position = 6)
-        game.play(position = 2)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-
-    @Test
-    fun `should finished when player has three in row of second horizontal`() {
-        game.play(position = 3)
-        game.play(position = 0)
-        game.play(position = 4)
-        game.play(position = 1)
-        game.play(position = 5)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-
-    @Test
-    fun `should finished when player has three in row of third horizontal`() {
-        game.play(position = 6)
-        game.play(position = 0)
-        game.play(position = 7)
-        game.play(position = 1)
-        game.play(position = 8)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-
-    @Test
-    fun `should finished when player has three in row of first vertical`() {
-        game.play(position = 0)
-        game.play(position = 1)
-        game.play(position = 3)
-        game.play(position = 2)
-        game.play(position = 6)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-
-    @Test
-    fun `should finished when player has three in row of second vertical`() {
-        game.play(position = 1)
-        game.play(position = 0)
-        game.play(position = 4)
-        game.play(position = 2)
-        game.play(position = 7)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-
-    @Test
-    fun `should finished when player has three in row of third vertical`() {
-        game.play(position = 1)
-        game.play(position = 0)
-        game.play(position = 4)
-        game.play(position = 2)
-        game.play(position = 7)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-
-    @Test
-    fun `should finished when player has three in diagonal`() {
-        game.play(position = 0)
-        game.play(position = 1)
-        game.play(position = 4)
-        game.play(position = 2)
-        game.play(position = 8)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-
-    @Test
-    fun `should finished when player has three in antidiagonal`() {
-        game.play(position = 2)
-        game.play(position = 1)
-        game.play(position = 4)
-        game.play(position = 3)
-        game.play(position = 6)
-        Assert.assertEquals(StateEnum.FINISHED, verifier.verify(board = game.state().board()))
-    }
-     */
