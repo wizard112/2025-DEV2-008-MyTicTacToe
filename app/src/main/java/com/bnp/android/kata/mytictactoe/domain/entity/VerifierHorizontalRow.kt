437216771cc0420271dc0240ad0ac78d1c4cee77
DEV2-008 <DEV2-008>
2025-08-10 18:07:13 +0200

    refactor(): add logic to verifier each row

--- app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierHorizontalRow.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierHorizontalRow.kt
@@ -5,6 +5,11 @@ import com.bnp.android.kata.mytictactoe.domain.interfaces.IVerifierRow
 
 class VerifierHorizontalRow: IVerifierRow {
     override fun verifierRow(board: Map<Int, MutableList<Player>>, player: Player): Boolean {
-        return true
+        val rowOne = board[0]
+        val rowTwo = board[1]
+        val rowThree = board[0]
+        return rowOne?.filter { it == player }?.size == 3
+                || rowTwo?.filter { it == player }?.size == 3
+                || rowThree?.filter { it == player }?.size == 3
     }
 }
