10db782fa98fcae8e3e03ec453c15028046b5288
DEV2-008 <DEV2-008>
2025-08-10 19:56:08 +0200

    refactor(): implement logic for diagonal

--- app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierDiagonalRow.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierDiagonalRow.kt
@@ -5,6 +5,18 @@ import com.bnp.android.kata.mytictactoe.domain.interfaces.IVerifierRow
 
 class VerifierDiagonalRow: IVerifierRow {
     override fun verifierRow(board: Map<Int, MutableList<Player>>, player: Player): Boolean {
-        return true
+        val list = mutableListOf<Player>()
+        var found = false
+        var i = 0
+        var j = board.size - 1
+        while (i < board.size && !found) {
+            board[i]?.let { players ->
+                if (j >= 0 && j < board.size && players[j] == player) list.add(element = players[j])
+            }
+            found = list.size == board.size
+            j--
+            i++
+        }
+        return found
     }
 }
