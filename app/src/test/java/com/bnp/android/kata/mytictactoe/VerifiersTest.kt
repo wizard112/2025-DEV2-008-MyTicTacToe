2924109085b2770889cb8f87b885de803a7353dd
DEV2-008 <DEV2-008>
2025-08-10 18:09:44 +0200

    test(): add test to case false - row has not three

--- app/src/test/java/com/bnp/android/kata/mytictactoe/VerifiersTest.kt
+++ app/src/test/java/com/bnp/android/kata/mytictactoe/VerifiersTest.kt
@@ -2,26 +2,26 @@ package com.bnp.android.kata.mytictactoe
 
 import com.bnp.android.kata.mytictactoe.domain.entity.VerifierHorizontalRow
 import com.bnp.android.kata.mytictactoe.domain.enums.Player
-import com.bnp.android.kata.mytictactoe.domain.enums.StateEnum
 import com.bnp.android.kata.mytictactoe.domain.interfaces.IVerifierRow
 import org.junit.Assert
-import org.junit.Before
 import org.junit.Test
 
 class VerifiersTest {
     private val boardOnlyRow: HashMap<Int, MutableList<Player>> = hashMapOf()
     private val verifierHorizontalRow: IVerifierRow = VerifierHorizontalRow()
 
-    @Before
-    fun setup() {
-        boardOnlyRow.put(0, mutableListOf(Player.X, Player.X, Player.X))
-        boardOnlyRow.put(1, mutableListOf(Player.X, Player.O, Player.EMPTY))
-        boardOnlyRow.put(2, mutableListOf(Player.X, Player.X, Player.X))
+
+    @Test
+    fun `should be true when try to get first row of the map`() {
+        boardOnlyRow.put(1, mutableListOf(Player.X, Player.X, Player.X))
+        val state = verifierHorizontalRow.verifierRow(board = boardOnlyRow, player = Player.X)
+        Assert.assertEquals(true, state)
     }
 
     @Test
-    fun `should has 1 2 3 when try to get first row of the map`() {
+    fun `should be false when try to get first row of the map`() {
+        boardOnlyRow.put(1, mutableListOf(Player.X, Player.O, Player.EMPTY))
         val state = verifierHorizontalRow.verifierRow(board = boardOnlyRow, player = Player.X)
-        Assert.assertEquals(true, state)
+        Assert.assertEquals(false, state)
     }
 }
