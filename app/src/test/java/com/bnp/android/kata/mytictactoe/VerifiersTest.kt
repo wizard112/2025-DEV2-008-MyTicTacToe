f5c71db7f731dc479cb5818d8a662439a5d95bcc
DEV2-008 <DEV2-008>
2025-08-10 18:25:52 +0200

    test(): add test for vertical verifier

--- app/src/test/java/com/bnp/android/kata/mytictactoe/VerifiersTest.kt
+++ app/src/test/java/com/bnp/android/kata/mytictactoe/VerifiersTest.kt
@@ -1,6 +1,7 @@
 package com.bnp.android.kata.mytictactoe
 
 import com.bnp.android.kata.mytictactoe.domain.entity.VerifierHorizontalRow
+import com.bnp.android.kata.mytictactoe.domain.entity.VerifierVerticalRow
 import com.bnp.android.kata.mytictactoe.domain.enums.Player
 import com.bnp.android.kata.mytictactoe.domain.interfaces.IVerifierRow
 import org.junit.Assert
@@ -9,7 +10,7 @@ import org.junit.Test
 class VerifiersTest {
     private val boardOnlyRow: HashMap<Int, MutableList<Player>> = hashMapOf()
     private val verifierHorizontalRow: IVerifierRow = VerifierHorizontalRow()
-
+    private val verifierVerticalRow: IVerifierRow = VerifierVerticalRow()
 
     @Test
     fun `should be true when try to get the second row of the map`() {
@@ -36,5 +37,14 @@ class VerifiersTest {
         boardOnlyRow.put(2, mutableListOf(Player.O))
         val state = verifierHorizontalRow.verifierRow(board = boardOnlyRow, player = Player.X)
         Assert.assertEquals(false, state)
+    }
+
+    @Test
+    fun `should be true when try to get the last column of the map`() {
+        boardOnlyRow.put(0, mutableListOf(Player.EMPTY, Player.EMPTY, Player.O))
+        boardOnlyRow.put(1, mutableListOf(Player.X, Player.X, Player.O))
+        boardOnlyRow.put(2, mutableListOf(Player.EMPTY, Player.EMPTY, Player.O))
+        val state = verifierVerticalRow.verifierRow(board = boardOnlyRow, player = Player.X)
+        Assert.assertEquals(true, state)
     }
 }
