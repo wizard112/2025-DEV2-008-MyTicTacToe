fa08d400ca37b2a708a6ad14e1b7345789baa9b5
DEV2-008 <DEV2-008>
2025-08-10 14:47:22 +0200

    test(): test if grid is displayed

--- app/src/androidTest/java/com/bnp/android/kata/mytictactoe/GameScreenTest.kt
+++ app/src/androidTest/java/com/bnp/android/kata/mytictactoe/GameScreenTest.kt
@@ -6,9 +6,7 @@ import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.printToLog
 import com.bnp.android.kata.mytictactoe.presentation.GameIntents
 import com.bnp.android.kata.mytictactoe.presentation.GameScreen
 import com.bnp.android.kata.mytictactoe.presentation.GameViewModel
@@ -30,6 +28,10 @@ class GameScreenTest {
         }
 
         composeTestRule.onNode(hasText("X is your turn"))
+        composeTestRule.onNode(hasText("X is your turn")).assertIsDisplayed()
+
+        composeTestRule.onNode(hasTestTag(testTag = "grid")).assertIsDisplayed()
+
         composeTestRule.onNode(hasTestTag(testTag = "cell_1")).assertIsDisplayed()
         composeTestRule.onNode(hasTestTag(testTag = "cell_1")).assertHasClickAction()
         composeTestRule.onNode(hasTestTag(testTag = "cell_1")).performClick()
