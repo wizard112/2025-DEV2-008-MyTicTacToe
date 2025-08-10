5450263ff26b380d07aaf8239ab1d1344fb694b4
DEV2-008 <DEV2-008>
2025-08-10 19:26:03 +0200

    refactor(): adapt logic in using map and any to verify horizontal row

--- app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierHorizontalRow.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/domain/entity/VerifierHorizontalRow.kt
@@ -5,12 +5,6 @@ import com.bnp.android.kata.mytictactoe.domain.interfaces.IVerifierRow
 
 class VerifierHorizontalRow: IVerifierRow {
     override fun verifierRow(board: Map<Int, MutableList<Player>>, player: Player): Boolean {
-        var found = false
-        var i = 0
-        while (i < board.size && !found) {
-            found = board[i]?.filter { it == player }?.size == board.size
-            i++
-        }
-        return found
+        return board.values.mapIndexed { i, p -> p.filter { it == player }.size == board.size }.any { it }
     }
 }
