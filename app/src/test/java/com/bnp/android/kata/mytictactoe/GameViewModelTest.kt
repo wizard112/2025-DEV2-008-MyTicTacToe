c193fbe03df137582e7077c7e7d5abcbd8665ab0
DEV2-008 <DEV2-008>
2025-08-11 00:00:29 +0200

    refactor(): adapt test to use column and row to take a position in the board

--- app/src/test/java/com/bnp/android/kata/mytictactoe/GameViewModelTest.kt
+++ app/src/test/java/com/bnp/android/kata/mytictactoe/GameViewModelTest.kt
@@ -25,28 +25,29 @@ class GameViewModelTest {
 
     @Test
     fun `should positions are filled when the players play`() {
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 0))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 0))
         val uiStateOne = gameViewModel.uiState.value
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 2))
         val uiStateTwo = gameViewModel.uiState.value
-        Assert.assertEquals(Player.O.name, uiStateOne.playerName)
-        Assert.assertEquals(Player.X.name, uiStateTwo.playerName)
+        Assert.assertEquals(Player.X.name, uiStateOne.playerName)
+        Assert.assertEquals(Player.O.name, uiStateTwo.playerName)
     }
 
     @Test
     fun `should can do nothing when the player O tries to taken a position already filled by player X`() {
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 1))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 1))
-        Assert.assertEquals(Player.X, gameViewModel.uiState.value.board[1])
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 1))
+        Assert.assertEquals(Player.X, gameViewModel.uiState.value.board[0]!![1])
     }
 
     @Test
     fun `should game is over when the player X wins`() {
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 0))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 1))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 4))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 2))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 8))
+        gameViewModel.handleIntents(intent = GameIntents.Starting)
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 0))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 1, row = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 2, row = 2))
         val uiState = gameViewModel.uiState.value
         Assert.assertTrue(uiState.winner)
         Assert.assertEquals(Player.X.name, uiState.playerName)
@@ -54,12 +55,13 @@ class GameViewModelTest {
 
     @Test
     fun `should game is over when the player O wins`() {
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 0))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 1))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 2))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 4))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 5))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 7))
+        gameViewModel.handleIntents(intent = GameIntents.Starting)
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 0))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 1, row = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 1, row = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 2, row = 1))
         val uiState = gameViewModel.uiState.value
         Assert.assertTrue(uiState.winner)
         Assert.assertEquals(Player.O.name, uiState.playerName)
@@ -67,15 +69,16 @@ class GameViewModelTest {
 
     @Test
     fun `should the game is draw when all nine squares are filled but without three x or O consecutive in horizontal - vertical - diagonal or antidiagonal`() {
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 0))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 2))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 6))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 3))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 8))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 4))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 5))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 7))
-        gameViewModel.handleIntents(intent = GameIntents.Moving(position = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Starting)
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 0))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 2, row = 0))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 1, row = 0))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 2, row = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 1, row = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 1, row = 2))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 2, row = 1))
+        gameViewModel.handleIntents(intent = GameIntents.Moving(column = 0, row = 1))
         val uiState = gameViewModel.uiState.value
         Assert.assertTrue(uiState.matchNul)
     }
