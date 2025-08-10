4aae1706e0268ad112830bfc90decefc4d5e5e26
DEV2-008 <DEV2-008>
2025-08-10 13:13:13 +0200

    test(): use printTolog to see the tree structure

--- app/src/androidTest/java/com/bnp/android/kata/mytictactoe/GameScreenTest.kt
+++ app/src/androidTest/java/com/bnp/android/kata/mytictactoe/GameScreenTest.kt
@@ -1,6 +1,11 @@
 package com.bnp.android.kata.mytictactoe
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
+import com.bnp.android.kata.mytictactoe.presentation.GameIntents
+import com.bnp.android.kata.mytictactoe.presentation.GameScreen
+import com.bnp.android.kata.mytictactoe.presentation.GameViewModel
 import org.junit.Rule
 import org.junit.Test
 
@@ -8,8 +13,16 @@ class GameScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
+    private val gameViewModel = GameViewModel()
+
     @Test
     fun testGameScreenComposable() {
+        gameViewModel.handleIntents(intent = GameIntents.Starting)
+        // Start the app
+        composeTestRule.setContent {
+            GameScreen(viewModel = gameViewModel)
+        }
 
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("TAG")
     }
 }
