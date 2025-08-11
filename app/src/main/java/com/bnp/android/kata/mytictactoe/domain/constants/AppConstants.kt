cdfa864464ddb8c61f4128189565e36e61b0f353
DEV2-008 <DEV2-008>
2025-08-11 20:06:42 +0200

    refactor(): modify constant

--- app/src/main/java/com/bnp/android/kata/mytictactoe/domain/constants/AppConstants.kt
+++ app/src/main/java/com/bnp/android/kata/mytictactoe/domain/constants/AppConstants.kt
@@ -1,6 +1,6 @@
 package com.bnp.android.kata.mytictactoe.domain.constants
 
-internal const val FIELD_ALREADY_TAKEN = "the position is already taken"
+internal const val FIELD_ALREADY_TAKEN = "the position"
 
 internal const val REF_PLAYERS = "players"
 internal const val REF_BOARD = "board"
