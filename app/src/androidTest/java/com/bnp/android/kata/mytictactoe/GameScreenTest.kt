0445eafd78ad5a97c01265fdec4c5fae7a6f3f73
DEV2-008 <DEV2-008>
2025-08-12 22:56:19 +0200

    refactor(): use the new format of board : column and row to test cell

--- app/src/androidTest/java/com/bnp/android/kata/mytictactoe/GameScreenTest.kt
+++ app/src/androidTest/java/com/bnp/android/kata/mytictactoe/GameScreenTest.kt
@@ -1,12 +1,17 @@
 package com.bnp.android.kata.mytictactoe
 
+import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.junit4.accessibility.enableAccessibilityChecks
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.tryPerformAccessibilityChecks
 import com.bnp.android.kata.mytictactoe.presentation.GameIntents
 import com.bnp.android.kata.mytictactoe.presentation.GameScreen
 import com.bnp.android.kata.mytictactoe.presentation.GameViewModel
@@ -28,14 +33,13 @@ class GameScreenTest {
         }
 
         composeTestRule.onNode(hasText("X is your turn"))
-        composeTestRule.onNode(hasText("X is your turn")).assertIsDisplayed()
 
-        composeTestRule.onNode(hasTestTag(testTag = "grid")).assertIsDisplayed()
+        composeTestRule.onNode(hasTestTag(testTag = "board")).assertIsDisplayed()
 
-        composeTestRule.onNode(hasTestTag(testTag = "cell_1")).assertIsDisplayed()
-        composeTestRule.onNode(hasTestTag(testTag = "cell_1")).assertHasClickAction()
-        composeTestRule.onNode(hasTestTag(testTag = "cell_1")).performClick()
-        composeTestRule.onNode(hasTestTag(testTag = "cell_1")).assertTextEquals("X")
+        composeTestRule.onNode(hasTestTag(testTag = "cell_01")).assertIsDisplayed()
+        composeTestRule.onNode(hasTestTag(testTag = "cell_01")).assertHasClickAction()
+        composeTestRule.onNode(hasTestTag(testTag = "cell_01")).performClick()
+        composeTestRule.onNode(hasTestTag(testTag = "cell_01")).assertTextEquals("X")
         composeTestRule.onNode(hasText("O is your turn"))
     }
 }
